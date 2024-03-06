6fbaf59a50952e105a1c4ecddaba37d256b66bd0
sebastian.gaviriag <sebastian.gaviriag@udea.edu.co>
2024-03-06 17:20:56 -0500

    Actualización de la clase Plots

--- Seguimiento_1.py
+++ Seguimiento_1.py
@@ -46,9 +46,8 @@ class Plots:
     self.sol1 = sol1
     self.sol2 = sol2
 
-  def plot(self, t, dt):
+  def plot(self, t, dt, option = "Trayectoria"):
     t_ = np.arange(0, t + dt, dt)
-
     x1 = [i[0] for i in self.sol1[0]]
     y1 = [i[1] for i in self.sol1[0]]
     z1 = [i[2] for i in self.sol1[0]]
@@ -56,22 +55,41 @@ class Plots:
     x2 = [i[0] for i in self.sol2[0]]
     y2 = [i[1] for i in self.sol2[0]]
     z2 = [i[2] for i in self.sol2[0]]
+    if option == "Trayectoria":
+      fig = plt.figure()
+      ax = fig.add_subplot(111, projection='3d')
+      ax.plot(x1, y1, z1)
+      ax.plot(x2, y2, z2)
 
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-    ax.plot(x1, y1, z1)
-    ax.plot(x2, y2, z2)
-
-    ax.quiver(x1[:-1], y1[:-1], z1[:-1], np.diff(x1), np.diff(y1), np.diff(z1), color='b', length=0.2, normalize=True)
-    ax.quiver(x2[:-1], y2[:-1], z2[:-1], np.diff(x2), np.diff(y2), np.diff(z2), color='r', length=0.2, normalize=True)
+      ax.quiver(x1[:-1], y1[:-1], z1[:-1], np.diff(x1), np.diff(y1), np.diff(z1), color='b', length=0.2, normalize=True)
+      ax.quiver(x2[:-1], y2[:-1], z2[:-1], np.diff(x2), np.diff(y2), np.diff(z2), color='r', length=0.2, normalize=True)
 
       # Etiquetas de los ejes
-    ax.set_xlabel('X')
-    ax.set_ylabel('Y')
-    ax.set_zlabel('Z')
+      ax.set_xlabel('X')
+      ax.set_ylabel('Y')
+      ax.set_zlabel('Z')
 
       # Mostrar la gráfica
-    plt.show()
+      plt.show()
+    
+    elif option == "x(t)":
+      fig, ax = plt.subplots(2, 3, figsize=(15, 10))
+      ax[0,0].plot(t_, x1, 'r')
+      ax[0,0].set_title('x_1(t)')
+      ax[0,1].plot(t_, y1, 'r')
+      ax[0,1].set_title('y_1(t)')
+      ax[0,2].plot(t_, z1, 'r')
+      ax[0,2].set_title('z_1(t)')
+      ax[1,0].plot(t_, x2, 'b')
+      ax[1,0].set_title('x_2(t)')
+      ax[1,1].plot(t_, y2, 'b')
+      ax[1,1].set_title('y_2(t)')
+      ax[1,2].plot(t_, z2, 'b')
+      ax[1,2].set_title('z_2(t)')
+
+      plt.tight_layout()
+
+      plt.show()
 
 
 
